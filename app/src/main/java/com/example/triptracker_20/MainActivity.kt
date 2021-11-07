0020e33f5c305b4247cfce6486ac5af9c9f90f28
EugenKoulik <evgen_koulik@mail.ru>
2021-11-07 03:36:42 +0300

    Current location button

--- app/src/main/java/com/example/triptracker_20/MainActivity.kt
+++ app/src/main/java/com/example/triptracker_20/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.triptracker_20
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import com.example.triptracker_20.screens.*
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -18,31 +21,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         replaceFragment(mapsFragment)
 
-        val bottom_Navigation = findViewById(R.id.bottom_navigation)  as com.google.android.material.bottomnavigation.BottomNavigationView
+        val bottomNavigation: BottomNavigationView = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_navigation)
 
-        bottom_Navigation.setOnNavigationItemReselectedListener {
+        bottomNavigation.setOnNavigationItemSelectedListener {
 
             when(it.itemId){
 
                 R.id.ic_filters -> replaceFragment(filtersFragment)
                 R.id.ic_map -> replaceFragment(mapsFragment)
                 R.id.ic_trips -> replaceFragment(tripsFragment)
-        }
+            }
             true
 
-    }
+        }
+
+
 
     }
 
     private fun replaceFragment(fragment: Fragment){
 
-        if(fragment != null){
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, fragment)
-            transaction.commit()
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.hide(mapsFragment)
+        transaction.hide(filtersFragment)
+        transaction.hide(tripsFragment)
+
+        if(!fragment.isAdded) {
+
+            transaction.add(R.id.fragment_container, fragment, "current fragment")
         }
+
+        transaction.show(fragment)
+        transaction.commit()
     }
 
 }
---
+++ app/src/main/java/com/example/triptracker_20/other/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.triptracker_20.other
+
+class Constants {
+
+    companion object{
+
+        const val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
+
+        const val REQUEST_CODE_LOCATION_PERMISSION = 0
+    }
+
+}
